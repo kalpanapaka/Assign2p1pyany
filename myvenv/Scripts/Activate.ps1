74107fdbabcf1ee5ad38e7fe8f3da751b7d3f2ad
Student <kpaka@unomaha.edu>
2019-08-08 14:19:32 -0500

    assign2part2

--- myvenv/Scripts/Activate.ps1
+++ myvenv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Assign2p1\myvenv"
+$env:VIRTUAL_ENV="c:\assign2p1\myvenv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
